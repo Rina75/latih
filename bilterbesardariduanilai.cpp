eddbe9cc62d591a15f0348f55840f4d91ab8f78e
Rina75 <waoderina75@gmail.com>
2022-11-01 20:59:54 +0800

    a

---
+++ bilterbesardariduanilai.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+int a,b;
+
+cout<<"Masukan Nilai a = ";cin>>a;
+cout<<"Masukan Nilai b = ";cin>>b;
+
+
+if (a>b){
+    cout<<"Nilai terbesar = "<<a;
+}else {
+    cout<<"Nilai terbesar = "<<b;
+}
+return 0;
+}
+
